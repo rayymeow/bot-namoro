19f2d9d4d2bb82d2d248ec8cc5ed374cd21ea5ad
rayymeow <rayymeow@gmail.com>
2026-04-07 16:11:47 -0300

    index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on('clientReady', async () => {
                     `\n<:catheart:1462162063722811424> ${days} dias\n<:cards:1462162167666311312> ${hours} horas\n<:dices:1462162235425030277> ${minutes} minutos`
                 )
                 .setColor(0xff69b4)
-                .setFooter({ text: "Desde 01/01/2026 às 00:02 <:ringw:1487594612217614518>" })
+                .setFooter({ text: "Desde 01/01/2026 às 00:02 <:ringw:1487594612217614518" })
                 .setTimestamp();
 
             await message.edit({ embeds: [embed] });
