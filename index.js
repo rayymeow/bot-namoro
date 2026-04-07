c20d7b953b0b7909480691b0d310b65ca6f6313b
rayymeow <rayymeow@gmail.com>
2026-04-07 15:53:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 client.on('ready', async () => {
     console.log(`Logado como ${client.user.tag}`);
-    console.log("ENTROU NO READY");
+    console.log("ENTROU");
 
-    const channel = await client.channels.fetch("1490517428055052401");
+    const channel = client.channels.cache.get("1490517428055052401");
 
-    console.log("Canal:", channel?.id);
+    console.log("Canal:", channel);
+
+    if (!channel) {
+        console.log("❌ Canal não encontrado no cache");
+        return;
+    }
 
     await channel.send("AGORA VAI FUNCIONAR");
 });
